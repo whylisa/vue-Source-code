bd6db691684406a245f308116e08215f99bcfbf1
whylisa <843655240@qq.com>
2019-08-05 00:16:17 +0800

    proxy

--- 数据绑定.js
+++ 数据绑定.js
@@ -19,3 +19,43 @@ while (i--) {
 	}
 	observe(data,true)
 }
+
+export function proxy(target: Object,sourceKey: string,key: string) {
+	sharedPropertyDefinition.get = function proxyGetter() {
+		return this.[sourceKey][key]
+	}
+	sharedPropertyDefinition.set = function proxySetter (val) {
+		this.[sourceKey][key] = val
+	}
+	Object.defineProperty(target,key,sharedPeopertyDefinition)
+}
+export class {
+	value: any;
+	dep: Dep;
+	vmCount: number;
+	constructor (value: any) {
+		this.value = value
+		this.dep = new Dep()
+		this.vmCount = 0
+		
+		def(value,'__ob__',this) 
+		if(Array.isArray(value)) {
+			const augument = hasProto?protoAugment: copyAugment,
+			augment(value,arrayMetheds,arrayKeys)
+			this.observeArray(value)
+		}else {
+			this.walk(value)
+		}
+	}
+	walk(obj: Object ) {
+		const keys = Object.keys(obj)
+		for(let i= 0;i< keys.length;i++) {
+			defineReactive(obj,keys[i],obj[keys[i]])
+		}
+	}
+	observeArray(item: Array<any>) {
+		for (let i = 0,l = items.length; i < l ;i++) {
+			observe(items[i])
+		}
+	}
+}
