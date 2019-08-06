c06899b8cc9fcf5b28757d9ce989b307a573e53d
haoyue.wang <843655240@qq.com>
2019-08-06 11:54:04 +0800

    event loop

--- nextTick.js
+++ nextTick.js
@@ -52,3 +52,27 @@ new Promise(resolve => {
 })
 
 console.log(2)
+setTimeout(function () {
+    console.log('setTimeout---0');
+}, 0);
+
+setTimeout(function () {
+    console.log('setTimeout---200');
+    setTimeout(function () {
+        console.log('inner-setTimeout---0');
+    });
+    Promise.resolve().then(function () {
+        console.log('promise5');
+    });
+}, 200);
+
+Promise.resolve().then(function () {
+    console.log('promise1');
+}).then(function () {
+    console.log('promise2');
+});
+Promise.resolve().then(function () {
+    console.log('promise3');
+});
+console.log('script end');
+
