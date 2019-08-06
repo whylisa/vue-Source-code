7b018e1a2dec6867ef5910c1c79fc8378c6e603a
haoyue.wang <843655240@qq.com>
2019-08-06 11:49:34 +0800

    nextTick

---
+++ nextTick.js
@@ -0,0 +1,54 @@
+宏任务: setTimeout
+I / O
+setInterval
+setImmediate
+主线程
+MessageChannel
+
+微任务: Promise系列.then.catch.finally
+process.nexTick
+MutationObserver
+
+setTimeout(() => {
+	console.log(8)
+}, 2000)
+
+setTimeout(() => {
+	console.log(3)
+	Promise.resolve().then(() => {
+		console.log(4)
+	})
+	setTimeout(() => {
+		console.log(6)
+	}, 3000)
+}, 1000)
+
+new Promise((resolve, reject) => {
+	console.log(5)
+}).then(() => {
+	console.log(7)
+})
+
+console.log(2)
+setTimeout(()=>{
+    console.log("setTimeout1");
+    Promise.resolve().then(data => {
+        console.log(222);
+    });
+});
+setTimeout(()=>{
+    console.log("setTimeout2");
+});
+Promise.resolve().then(data=>{
+    console.log(111);
+});
+setTimeout(_ => console.log(4))
+
+new Promise(resolve => {
+  resolve()
+  console.log(1)
+}).then(_ => {
+  console.log(3)
+})
+
+console.log(2)
